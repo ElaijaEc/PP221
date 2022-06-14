00ffcc539273de1dc45e9e09a8b96eb543ca376f
Vladimir Petrov <ElaijaEc@yandex.ru>
2022-06-14 21:03:31 +0400

    V4.0

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,37 +1,36 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
+   @PersistenceContext
+   private Session session;
 
-   private SessionFactory sessionFactory;
-   @Autowired
-   public UserDaoImp(SessionFactory sessionFactory) {
-      this.sessionFactory = sessionFactory;
-   }
    @Override
    public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
+      session.save(user);
    }
 
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query=session.createQuery("from User");
       return query.getResultList();
    }
 
    @Override
    public User getUserByCar(int series, String model) {
-      TypedQuery<User> query = sessionFactory.getCurrentSession()
-              .createQuery("from User user where car.model = :model and car.series = :series");
+      TypedQuery<User> query = session.createQuery("from User user where car.model = :model and car.series = :series");
       query
               .setParameter("series",series)
               .setParameter("model",model);
